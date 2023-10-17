7c853954d874e8100d6c2070379cd582a4f69305
Naina <nainavishwakarma115@gmail.com>
2023-10-17 21:52:55 +0530

    add user login page

--- oba/apis/pages/admin/user_login.php
+++ oba/apis/pages/admin/user_login.php
@@ -1,18 +1,76 @@
+<?php 
+require_once("../../common/database.php");
+$db = new Database();
+$conn = $db->connect();
+// Receive username and password from POST request
+if(isset($_POST['signin'])){
+$username = $_POST['username'];
+$password = $_POST['password'];
+// Hash the received password
+//$hashedPassword = password_hash($password, PASSWORD_BCRYPT);
+// Query the database to check if username and password match
+$stmt = $conn->prepare("SELECT * FROM user WHERE username = ? LIMIT 1");
+$stmt->bind_param("s",$username);
+$stmt->execute();
+$result = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+if(count($result) > 0){
+ if (password_verify($password, $result[0]['password'])){
+        // Username and password match
+        $token = bin2hex(random_bytes(32)); // Generate a random token
+        $tokenCreationTime = date("Y-m-d H:i:s"); // Current timestamp
+        // Store token and creation time in the database
+        $insertQuery = "UPDATE  user SET token = ? ,token_creation_time = ? WHERE id = ?";
+        $insertStmt = $conn->prepare($insertQuery);
+        $insertStmt->bind_param('ssi', $token,$tokenCreationTime,$result[0]['id']);
+        
+        $insertStmt->execute();
+
+        //Send success response to frontend
+        echo json_encode(["token" => $token]);
+        session_start();
+        $_SESSION["s_id"] =$result[0]['id'];
+        $_SESSION["s_username"] =$result[0]['username'];
+        $_SESSION["s_role"] =$result[0]['role'];
+        $_SESSION["s_token"] =$result[0]['token'];
+        echo $_SESSION["s_token"];
+        $_SESSION["s_username"];
+
+      
+    }else {
+        // Invalid username or password
+        echo json_encode(["error" => "Invalid username or password "]);
+        $error = "Invalid Username Password";
+    }
+  }
+else {
+  // Username not found
+  echo json_encode(["error" => "Invalid username or password"]);
+   }
+  
+// Close the database connection
+$stmt->close();
+
+
+$conn->close();
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Log in</title>
+  <title>Harihar  | Log in</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="./plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="../../theme/plugins/fontawesome-free/css/all.min.css">
   <!-- icheck bootstrap -->
-  <link rel="stylesheet" href="./plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="../../theme/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="./dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="../../theme/dist/css/adminlte.min.css">
 </head>
 <body class="hold-transition login-page">
 <div class="login-box">
@@ -24,7 +82,7 @@
     <div class="card-body login-card-body">
       <p class="login-box-msg">Sign in to start your session</p>
 
-      <form action="#" method="post" id="loginform">
+      <form action="<?php echo $_SERVER["PHP_SELF"];?>" method="post" id="loginform">
         <div class="input-group mb-3">
           <input type="text" class="form-control" placeholder="Username" name="username">
           <div class="input-group-append">
@@ -34,7 +92,7 @@
           </div>
         </div>
         <div class="input-group mb-3">
-          <input type="password" class="form-control" placeholder="Password" name="pwd">
+          <input type="password" class="form-control" placeholder="Password" name="password">
           <div class="input-group-append">
             <div class="input-group-text">
               <span class="fas fa-lock"></span>
@@ -52,7 +110,7 @@
           </div>
           <!-- /.col -->
           <div class="col-4">
-            <button type="submit" class="btn btn-primary btn-block" id="signin">Sign In</button>
+            <button type="submit" class="btn btn-primary btn-block" id="signin" name="signin">Sign In</button>
           </div>
           <!-- /.col -->
         </div>
@@ -82,31 +140,11 @@
 <!-- /.login-box -->
 
 <!-- jQuery -->
-<script src="./plugins/jquery/jquery.min.js"></script>
+<script src="../../theme/plugins/jquery/jquery.min.js"></script>
 <!-- Bootstrap 4 -->
-<script src="./plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="../../theme/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <!-- AdminLTE App -->
-<script src="./dist/js/adminlte.min.js"></script>
-<script>
-    //Function For Convert From Data to Json object
-    function jsonData(targetForm){
-        var arr = $(targetForm).serializeArray();
-        //console.log(arr);
-        var obj = {};
-        for(var a=0; a < arr.length; a++){
-            obj[arr[a].name] = arr[a].value;
-        }
-        //console.log(obj);
-        var json_str = JSON.stringify(obj);
-       // console.log(json_str);
-       return json_str;
-    }
-    $("#signin").on("click",function(e){
-        e.preventDefault();
-       var jsonobj_login = jsonData("#loginform");
-       console.log(jsonobj_login);
-
-    })
-</script>
+<script src="../../theme/dist/js/adminlte.min.js"></script>
+  
 </body>
 </html>
