ea434ad3e445eff78304a8da0fcccf5ad2f221fe
Naina <nainavishwakarma115@gmail.com>
2023-10-20 15:54:35 +0530

    edit admin log page

--- oba/apis/pages/admin/user_login.php
+++ oba/apis/pages/admin/user_login.php
@@ -81,7 +81,7 @@ $conn->close();
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="#"><b>Admin</b>LTE</a>
+    <a href="#"><b>Harihar</b></a>
   </div>
   <!-- /.login-logo -->
   <div class="card">
@@ -108,14 +108,7 @@ $conn->close();
         <span class="text-danger"><?php echo $error; ?></span>
        
         <div class="row">
-          <div class="col-8">
-            <div class="icheck-primary">
-              <input type="checkbox" id="remember">
-              <label for="remember">
-                Remember Me
-              </label>
-            </div>
-          </div>
+         
           <!-- /.col -->
           <div class="col-4">
             <button type="submit" class="btn btn-primary btn-block" id="signin" name="signin">Sign In</button>
@@ -124,23 +117,10 @@ $conn->close();
         </div>
       </form>
 
-      <div class="social-auth-links text-center mb-3">
-        <p>- OR -</p>
-        <a href="#" class="btn btn-block btn-primary">
-          <i class="fab fa-facebook mr-2"></i> Sign in using Facebook
-        </a>
-        <a href="#" class="btn btn-block btn-danger">
-          <i class="fab fa-google-plus mr-2"></i> Sign in using Google+
-        </a>
-      </div>
+      
       <!-- /.social-auth-links -->
 
-      <p class="mb-1">
-        <a href="forgot-password.html">I forgot my password</a>
-      </p>
-      <p class="mb-0">
-        <a href="register.html" class="text-center">Register a new membership</a>
-      </p>
+     
     </div>
     <!-- /.login-card-body -->
   </div>
