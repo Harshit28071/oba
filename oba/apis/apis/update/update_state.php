20144f94a4bf54335d6cc02767e80b546324895f
Naina <nainavishwakarma115@gmail.com>
2023-10-24 16:35:59 +0530

    UPDATE STATE UPDATE API

--- oba/apis/apis/update/update_state.php
+++ oba/apis/apis/update/update_state.php
@@ -1,15 +1,21 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Control-Allow-Origin: *');
+header('Access-Controle-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+$data = json_decode(file_get_contents("php://input"),true);
+$state_update =$data['editstatename'];
+$state_id =$data['editidstate'];
 include('../../common/database.php');
 $db = new Database();
 $conn = $db->connect();
-
 $stmt = $conn->prepare("update `state` set `state` = ? where id = ?");
-$stmt->bind_param("si",$_GET["state"],$_GET["id"]);
+$stmt->bind_param("si",$state_update,$state_id);
 $stmt->execute();
 $id = $stmt->affected_rows;
 $stmt->close();
 
 $conn->close();
-
-echo $id;
+echo json_encode($id);
+//echo $id;
 ?>
