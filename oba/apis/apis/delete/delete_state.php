078603344ad8a9e6eae5c7118fbb2b7d8c7536ff
Naina <nainavishwakarma115@gmail.com>
2023-10-24 16:34:48 +0530

    UPDATE STATE DELETE API

--- oba/apis/apis/delete/delete_state.php
+++ oba/apis/apis/delete/delete_state.php
@@ -1,15 +1,20 @@
 <?php
+header('Content-Type: application/json');
+header('Access-Controle-Allow-Methods: POST');
+//header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+$data = json_decode(file_get_contents("php://input"),true);
+$state_id =$data['removestateid'];
 include('../../common/database.php');
 $db = new Database();
 $conn = $db->connect();
 
 $stmt = $conn->prepare("delete from `state` where id = ?");
-$stmt->bind_param("i",$_GET["id"]);
+$stmt->bind_param("i",$state_id);
 $stmt->execute();
 $id = $stmt->affected_rows;
 $stmt->close();
 
 $conn->close();
-
-echo $id;
+echo json_encode($id);
+//echo $id;
 ?>
