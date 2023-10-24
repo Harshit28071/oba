413bec38984a76d19257170825685cbcbfe49ed4
Naina <nainavishwakarma115@gmail.com>
2023-10-24 12:14:05 +0530

    edit add role

--- oba/apis/apis/add/add_role.php
+++ oba/apis/apis/add/add_role.php
@@ -1,14 +1,15 @@
 <?php
-//require_once("../../common/check_token.php");
-header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Controle-Allow-Methods: POST');
-header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
-$data = json_decode(file_get_contents("php://input"),true);
-$role = $data['Addrole'];
+session_start();
 include('../../common/database.php');
 $db = new Database();
 $conn = $db->connect();
+$urlredirect ="../../pages/admin/user_login.php";
+include("../../common/check_token.php");
+header('Content-Type: application/json');
+header('Access-Controle-Allow-Methods: POST');
+// header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,Access-Control-Allow-Methods,Authorization,X-Requested-With');
+$data = json_decode(file_get_contents("php://input"),true);
+$role = $data['Addrole'];
 $stmt = $conn->prepare("INSERT INTO `roles`(`role`) VALUES (?)");
 $stmt->bind_param("s",$role);
 $stmt->execute();
