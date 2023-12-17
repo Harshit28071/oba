11e957fcae859439cc619b8e7e923b5114ebbe4d
harshitoba <harshit.agarwal@hariharfood.in>
2023-12-17 12:21:13 +0530

    moving files

--- oba/apis/apis/select/admin/fetch_single_product_multiimg.php
+++
@@ -1,8 +0,0 @@
-<?php
-header('Content-Type: application/json');
-$data = json_decode(file_get_contents("php://input"),true);
-$product_id = $data['p_id'];
-include('../../../common/database.php');
-$db = new Database();
-$conn = $db->connect();
-?>
